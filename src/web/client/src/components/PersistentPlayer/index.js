84b95eb2f2aa13fe6bb594c50526d7bf6865034a
HaskaZuki <oyyi2005@gmail.com>
2026-02-22 15:35:15 +0700

    fix: correct import path for SocketContext in PersistentPlayer

--- src/web/client/src/components/PersistentPlayer/index.js
+++ src/web/client/src/components/PersistentPlayer/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { useSocket } from '../context/SocketContext';
+import { useSocket } from '../../context/SocketContext';
 import './PersistentPlayer.css';
 
 function PersistentPlayer() {
