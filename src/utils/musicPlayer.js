b198d5574b20e236fa2d6bc9062c78912804396e
HaskaZuki <oyyi2005@gmail.com>
2026-03-16 15:13:32 +0700

    fix(lavalink): prioritize spsearch and fallback lavasrc to soundcloud

--- src/utils/musicPlayer.js
+++ src/utils/musicPlayer.js
@@ -136,9 +136,9 @@ module.exports = {
             }
         } else {
             const searchSources = [
+                { name: 'Spotify', prefix: 'spsearch' },
                 { name: 'YouTube Music', prefix: 'ytmsearch' },
                 { name: 'YouTube', prefix: 'ytsearch' },
-                { name: 'Spotify', prefix: 'spsearch' },
                 { name: 'SoundCloud', prefix: 'scsearch' }
             ];
             for (const source of searchSources) {
@@ -303,7 +303,7 @@ module.exports = {
                     const currentSource = playerState.currentTrack.info.sourceName;
                     const searchQuery = `${trackAuthor} ${trackTitle}`;
                     console.log(` Trying to find alternative for "${trackTitle}" (failed source: ${currentSource})`);
-                    const altSources = ['ytmsearch', 'ytsearch', 'spsearch', 'scsearch']
+                    const altSources = ['spsearch', 'ytmsearch', 'ytsearch', 'scsearch']
                         .filter(s => !currentSource?.includes(s.replace('search', '')));
                     for (const sourcePrefix of altSources) {
                         try {
